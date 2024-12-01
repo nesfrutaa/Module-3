3087493d8f890930cefa0aca6ea83ed2d34606fe
Clarisse Bajaro <138785959+nesfrutaa@users.noreply.github.com>
2024-12-01 23:41:09 +0800

    Create MyToken.sol

---
+++ MyToken.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+    constructor() ERC20("Clarisse", "202111295") Ownable(msg.sender) {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(_msgSender(), amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        require(balanceOf(_msgSender()) >= amount, "Transfer failed: insufficient balance");
+
+        return super.transfer(recipient, amount);
+    }
+}
